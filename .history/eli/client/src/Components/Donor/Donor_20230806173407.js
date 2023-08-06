cd3895c01a7cf293205b0a7336ba247b576f839e
Whygal <yanivgalili18@gmail.com>
2023-08-06 17:51:40 +0300

    context

--- .history/eli/client/src/Components/Donor/Donor_20230806173407.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Donor = () => {
-  return (
-    <div>Donor</div>
-  )
-}
-
-export default Donor
--- .history/eli/client/src/context_20230806173633.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const Context1 = createContext(null);
-
-export default Context1;

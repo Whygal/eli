4f69190eb27cc0a3c1ae5657f8a6e221674e7fd0
rachelsin <rmpodo@gmail.com>
2023-08-09 16:58:45 +0300

    youytube video

--- client/src/Components/Donor/DonorCard.js
+++ client/src/Components/Donor/DonorCard.js
@@ -3,11 +3,14 @@ import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
 import { Link } from "react-router-dom";
 import { Card } from 'react-bootstrap';
+import Hidden from '@mui/material/Hidden';
+
 
 function DonorCard({ index, name, amount, groupId, groupName, comment }) {
     return (
         <>
             <span className='col-md-6 px-3 py-2' key={index}>
+                <Hidden mdDown>
                 <Card className='h-100 shadow rounded'>
                     <div className='p-3'>
                         <Grid container spacing={1}>
@@ -27,6 +30,28 @@ function DonorCard({ index, name, amount, groupId, groupName, comment }) {
                         </Grid>
                     </div>
                 </Card>
+                </Hidden>
+                <Hidden mdUp>
+                     <Card className='shadow rounded'>
+                    <div className='p-3'>
+                        <Grid container spacing={1}>
+                            <Grid item xs="auto">
+                                <Avatar>{name.charAt(0).toUpperCase()}</Avatar>
+                            </Grid>
+                            <Grid item xs={8} md={8}>
+                                <h5 className='p-2'>{name}</h5>
+                                <p className=''>{comment}</p>
+                                <p className='text-secondary'>
+                                    ע"י <span className=''><Link className='text-decoration-none hover-link text-secondary' to={`/${groupId}/${groupName}`}>{groupName}</Link></span>
+                                </p>
+                            </Grid>
+                            <Grid item xs="auto">
+                                <p className='py-2'>₪{amount}</p>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Card>
+                </Hidden>
             </span>
         </>
     );

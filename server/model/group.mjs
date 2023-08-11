c827ed9ee0aee2d90112c5fc3bf92f85f742b159
rachelsin <rmpodo@gmail.com>
2023-08-11 12:39:03 +0300

    nameHebrew

--- server/model/group.mjs
+++ server/model/group.mjs
@@ -1,28 +1,36 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 const GroupSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 255,
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 255,
+  },
+  nameHebrew: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 255,
+  },
+  goal: {
+    type: Number,
+    required: true,
+  },
+  donors: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Donor",
     },
-    goal: {
-        type: Number,
-        required: true,
-    },
-    donors: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Donor',
-    }],
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const GroupModel = mongoose.model('Group', GroupSchema);
+const GroupModel = mongoose.model("Group", GroupSchema);
 
 export default GroupModel;
